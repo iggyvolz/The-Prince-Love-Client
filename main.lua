b0451468cf7c6436f5d26bfafddee51e17904941
iggyvolz <iggyvolz@aim.com>
2015-04-04 10:59:48 -0400

    FIX STUFF

--- main.lua
+++ main.lua
@@ -1,25 +1,16 @@
-local function explode(div,str) -- credit: http://richard.warburton.it
-  if (div=='') then return false end
-  local pos,arr = 0,{}
-  -- for each divider found
-  for st,sp in function() return string.find(str,div,pos,true) end do
-    table.insert(arr,string.sub(str,pos,st-1)) -- Attach chars left of current divider
-    pos = sp + 1 -- Jump past current divider
-  end
-  table.insert(arr,string.sub(str,pos)) -- Attach chars right of last divider
-  return arr
-end
 local socket = require "socket"
 local address,port="127.0.0.1",1897
 local updaterate=5
 local t=0
 local udp
+local text=""
 function love.load()
-    udp = socket.udp()
-    udp:settimeout(0)
-    udp:setpeername(address, port)
-    math.randomseed(os.time())
-    udp:send("invade poland 1")
+  socket = require "socket"
+  address,port="127.0.0.1",1897
+  udp = socket.udp()
+  udp:settimeout(0.1)
+  udp:setpeername(address, port)
+  udp:send("CONNECT")
 end
 
 function love.update(deltatime)
@@ -31,5 +22,29 @@ function love.update(deltatime)
     local data=udp:receive()
     if data then
       print(data)
+      text=text..data
     end
+end
+
+function love.mousepressed(x, y, button)
+  if button=="l" and x>20 and x<70 and y>20 and y<70 then
+    print("X")
+    udp:send("invade poland 1")
+  end
+end
+local _font = {
+
+}
+local function font(size)
+  if _font[size] then return _font[size] end
+  _font[size] = love.graphics.newFont(size)
+  return _font[size]
+end
+
+function love.draw()
+  love.graphics.setBackgroundColor(255,255,255)
+  love.graphics.setColor(0, 0, 0)
+  love.graphics.setFont(font(36))
+  love.graphics.rectangle("line",20,20,50,50)
+  love.graphics.printf(text,0,80,800)
 end
